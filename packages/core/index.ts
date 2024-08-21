b8658882f2fa039bd5efeb0fc5993a4671b59e3b
Giacomo Citi <giacomo.citi@zazuko.com>
2024-08-21 12:25:37 +0200

    commands may leverage previous ones in same file

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -106,23 +106,28 @@ function toGraphs(api: string) {
 async function applyUpdates(api: string, validDirs: string[], dataset: DatasetCore) {
   const engine = new QueryEngine()
   const store = new Store([...dataset])
-  const destination = new Store([...dataset])
+  const results = $rdf.dataset()
   for (const dir of validDirs) {
     for await (const file of walk(dir)) {
-      if (file.endsWith('.ru')) {
-        const relative = path.relative(dir, file)
-        log.debug('Applying updates from %s, dataset size %s', relative, store.size)
-        const query = fs.readFileSync(file, 'utf-8')
-        await engine.queryVoid(query, {
-          sources: [store],
+      if (!file.endsWith('.ru')) {
+        continue
+      }
+      const destination = new Store()
+      const relative = path.relative(dir, file)
+      log.debug('Applying updates from %s, dataset size %s', relative, store.size)
+      const query = fs.readFileSync(file, 'utf-8')
+      for (const command of query.split(';')) {
+        await engine.queryVoid(command, {
+          sources: [destination, store],
           destination,
           baseIRI: baseIRI(relative, api),
         })
-        log.debug('Applied updates from %s, dataset size %s', relative, store.size)
       }
+      log.debug('Applied updates from %s, dataset size %s', relative, store.size)
+      results.addAll(destination)
     }
   }
-  return $rdf.dataset(destination)
+  return $rdf.dataset([...dataset, ...results])
 }
 
 function isValidDir(dir: string) {
