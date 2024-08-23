d79b24703c9c563908db69241e2e12cba91521ea
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-08-23 10:05:16 +0200

    fix: splitting queries

--- packages/core/lib/applyUpdates.ts
+++ packages/core/lib/applyUpdates.ts
@@ -4,9 +4,12 @@ import { walk } from '@fcostarodrigo/walk'
 import { QueryEngine } from '@comunica/query-sparql'
 import { Store } from 'n3'
 import type { DatasetCore } from '@rdfjs/types'
+import { translate } from 'sparqlalgebrajs'
+import type { Operation } from 'sparqlalgebrajs/lib/algebra.js'
+import { types } from 'sparqlalgebrajs/lib/algebra.js'
 import $rdf from '../env.js'
 import log from './log.js'
-import { baseIRI } from './baseIRI.js'
+import { baseIRI as getBaseIRI } from './baseIRI.js'
 
 export async function applyUpdates(api: string, validDirs: string[], dataset: DatasetCore) {
   const engine = new QueryEngine()
@@ -19,13 +22,16 @@ export async function applyUpdates(api: string, validDirs: string[], dataset: Da
       }
       const destination = new Store()
       const relative = path.relative(dir, file)
+      const baseIRI = getBaseIRI(relative, api)
       log.debug('Applying updates from %s, dataset size %s', relative, store.size)
       const query = fs.readFileSync(file, 'utf-8')
-      for (const command of query.split(';')) {
+
+      const algebra = translate(query, { quads: true, baseIRI })
+      for (const command of getUpdates(algebra)) {
         await engine.queryVoid(command, {
           sources: [destination, store],
           destination,
-          baseIRI: baseIRI(relative, api),
+          baseIRI,
         })
       }
       log.debug('Applied updates from %s, dataset size %s', relative, store.size)
@@ -33,4 +39,16 @@ export async function applyUpdates(api: string, validDirs: string[], dataset: Da
     }
   }
   return $rdf.dataset([...dataset, ...results])
+}
+
+function getUpdates(query: Operation) {
+  switch (query.type) {
+    case types.COMPOSITE_UPDATE:
+      return query.updates
+    case types.DELETE_INSERT:
+      return [query]
+    default:
+      log.debug('Only update queries are allowed')
+      return []
+  }
 }
